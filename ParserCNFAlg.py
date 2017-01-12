b73aee79a0e73f6eacb639367c121bd973a54e0a
Logan Pletcher <lpletch95@hotmail.com>
2017-01-12 07:02:07 -0600

    Update

--- ParserCNFAlg.py
+++ ParserCNFAlg.py
@@ -1,17 +1,33 @@
-f = open('butz.txt')
+filename = input("enter the name of your file: ")
+f = open(filename+'.txt')
 line = 'empty'
 
-expressionList = []
 literalList = []
 literalCount = []
 clauseCount = []
-
+expressionList = 0
 while (line != ''):
     line = f.readline()
-    for i in line:
-        print i
-    '''
-    if (line[0] == '('
-        and line[len(line)-1] == ')'):
-        print line
-'''
+    expressionList += line.count('\n')
+print("Number of Expressions", expressionList)
+f.seek(0)
+for i in range (0, expressionList):
+    print("-------------------------------------------------")
+    newLiterals = []
+    literalList.append(newLiterals)
+    tempClause = 0
+    line = f.readline()
+    tempClause = line.count('(')
+    clauseCount.append(tempClause)
+    print(line, "\nNumber of Clauses", clauseCount[i])
+    tempLiteral = 0
+    for x in line:
+        if(x.isalpha()):
+            if not(x in literalList[i]):
+                literalList[i].append(x)
+                tempLiteral += 1
+    literalCount.append(tempLiteral)
+    print("List of Literals:", literalList[i])
+    print("Number of Literals", literalCount[i])
+    i += 1
+print("-------------------------------------------------")
