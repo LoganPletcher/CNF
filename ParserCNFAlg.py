9e580b74d3da5d2295d2c6af5431ed8c73986d5c
Logan Pletcher <lpletch95@hotmail.com>
2017-01-17 17:46:23 -0600

    Started working on having the parser evaluate the expressions

--- ParserCNFAlg.py
+++ ParserCNFAlg.py
@@ -1,3 +1,4 @@
+import random
 filename = input("enter the name of your file: ")
 f = open(filename+'.txt')
 line = 'empty'
@@ -6,6 +7,7 @@ literalList = []
 literalCount = []
 clauseCount = []
 expressionList = 0
+valueList = []
 while (line != ''):
     line = f.readline()
     expressionList += line.count('\n')
@@ -29,5 +31,28 @@ for i in range (0, expressionList):
     literalCount.append(tempLiteral)
     print("List of Literals:", literalList[i])
     print("Number of Literals", literalCount[i])
+    literalValues = []
+    for y in range (0, literalCount[i]):
+        literalValues.append(random.randrange(0,2))
+    valueList.append(literalValues)
+    print("List of Literal Values:", valueList[i])
+    finalValue = False
+    trueValues = []
+    trueValues2 = []
+    for x in range (0,len(line)):
+        if(line[x].isalpha()):
+            k = 0
+            for j in literalList[i]:
+                if line[x] == j:
+                    tempValue = valueList[i][k]
+                    if line[x-1] == '!':
+                        if tempValue == 1:
+                            tempValue = 0
+                        elif tempValue == 0:
+                            tempValue = 1
+                    trueValues.append(tempValue)
+                if line[x-2] == '|':
+                    
+                k += 1
     i += 1
 print("-------------------------------------------------")
